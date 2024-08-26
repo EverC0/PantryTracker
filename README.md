439e8769913e3b486b2a2346ccf61f5d821ce78d
EverC0 <everrjc@gmail.com>
2024-08-26 13:04:36 -0400

    background

--- README.md
+++ README.md
@@ -2,6 +2,5 @@ App is deployed on vercel: https://pantry-tracker-pi-flame.vercel.app
 
 First, run the development server:
 
-```bash
 npm run dev
 # or
--- app/components/Header.js
+++ app/components/Header.js
@@ -5,8 +5,17 @@ import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    custom: {
+      main: 'inherit', // Define your custom color here
+    },
+  },
+});
+
 const Header = () => {
-  // Use next/navigation instead of next/router
   const router = useRouter();
 
   const handleNavigation = (path) => {
@@ -15,25 +24,27 @@ const Header = () => {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Pantry Tracker
-        </Typography>
-        <Button color="inherit" onClick={() => handleNavigation('/')}>
-          Home
-        </Button>
-        <Button color="inherit" onClick={() => handleNavigation('/pantry')}>
-          Pantry
-        </Button>
-        <Button color="inherit" onClick={() => handleNavigation('/Viewall')}>
-          View
-        </Button>
-        <Button color="inherit" onClick={() => handleNavigation('/LogOff')}>
-          LogOff
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" color="custom">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Pantry Tracker
+          </Typography>
+          <Button color="inherit" onClick={() => handleNavigation('/')}>
+            Home
+          </Button>
+          <Button color="inherit" onClick={() => handleNavigation('/pantry')}>
+            Pantry
+          </Button>
+          <Button color="inherit" onClick={() => handleNavigation('/Viewall')}>
+            View
+          </Button>
+          <Button color="inherit" onClick={() => handleNavigation('/LogOff')}>
+            LogOff
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
